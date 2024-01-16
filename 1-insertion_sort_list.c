cfeb8d267bd3d2afe14d500ef1b109855008ef7c
savvyh <7277@holbertonstudents.com>
2024-01-16 07:54:03 +0000

    fix the betty style on the file insertion_sort_list.c

--- 1-insertion_sort_list.c
+++ 1-insertion_sort_list.c
@@ -3,42 +3,44 @@
 /**
  * insertion_sort_list - Sorts a doubly linked list in ascending order
  * @list: Pointer to a pointer to the head of the doubly linked list
+ *
+ * Return: Nothing
  */
 void insertion_sort_list(listint_t **list)
 {
-    listint_t *current, *next_node;
+	listint_t *current, *next_node;
 
-    if (list == NULL || *list == NULL)
-        return;
+	if (list == NULL || *list == NULL)
+		return;
 
-    current = (*list)->next;
+	current = (*list)->next;
 
-    while (current != NULL)
-    {
-        next_node = current->next;
+	while (current != NULL)
+	{
+		next_node = current->next;
 
-        while (current->prev != NULL && current->n < current->prev->n)
-        {
-            /* Swap nodes */
-            current->prev->next = current->next;
-            if (current->next != NULL)
-                current->next->prev = current->prev;
+		while (current->prev != NULL && current->n < current->prev->n)
+		{
+			/* Swap nodes */
+			current->prev->next = current->next;
+			if (current->next != NULL)
+				current->next->prev = current->prev;
 
-            current->next = current->prev;
-            current->prev = current->prev->prev;
+			current->next = current->prev;
+			current->prev = current->prev->prev;
 
-            if (current->prev != NULL)
-                current->prev->next = current;
+			if (current->prev != NULL)
+				current->prev->next = current;
 
-            current->next->prev = current;
+			current->next->prev = current;
 
-            /* Update head if necessary */
-            if (current->prev == NULL)
-                *list = current;
+			/* Update head if necessary */
+			if (current->prev == NULL)
+				*list = current;
 
-            print_list(*list);
-        }
+			print_list(*list);
+		}
 
-        current = next_node;
-    }
+		current = next_node;
+	}
 }
